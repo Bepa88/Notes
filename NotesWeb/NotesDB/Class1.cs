b6295f6bff9e5f40e0726d13b4e72203198cc3e4
Bepa88 <leleiko.vira@gmail.com>
2020-01-09 15:10:21 +0200

    Add project in repositories

---
+++ NotesWeb/NotesDB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesDB
+{
+    public class Class1
+    {
+    }
+}
